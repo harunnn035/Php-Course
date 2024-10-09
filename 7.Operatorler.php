37a9204c749bd60196858f35c319d1154cc4a073
harunnn035 <harundag0101@gmail.com>
2024-10-09 12:12:55 +0300

    Create 7.Operatorler.php

---
+++ 7.Operatorler.php
@@ -0,0 +1,19 @@
+<!-- Aritmetik Operatorler -->
+<?php
+$num1=10;
+$num2=2;
+
+echo $num1. "**".$num2."=? <br>";
+echo "sonuç:".$num1**$num2;
+?>
+
+
+<!-- Atama Opertorler -->
+<?php
+$num1=10;
+$num2=2;
+
+echo $num1. "**".$num2."=? <br>";
+echo "sonuç:".$num1**$num2;
+?>
+
