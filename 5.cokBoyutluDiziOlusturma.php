6c9a09568f1b81a2adf162b4664a38156f01a51f
harunnn035 <harundag0101@gmail.com>
2024-10-09 11:39:47 +0300

    update

--- 5.cokBoyutluDiziOlusturma.php
+++ 5.cokBoyutluDiziOlusturma.php
@@ -1,10 +1,14 @@
 <?php
 $student=
 [
-    ["harn","ahmt","mehmr","okan"],
+    ["ali","ayse","osman","mehtap","zehra"],
     ["erkek","kadin"],
     ["1A","1B","1C"]
 ];
-    echo $student[2][1]."<br>".$student[1][0]."<br>".$student[2][0]."<br>";
+    echo $student[0][0]." ".$student[1][0]." ".$student[2][0]."<br>";
+    echo $student[0][1]." ".$student[1][1]." ".$student[2][1]."<br>";
+    echo $student[0][2]." ".$student[1][0]." ".$student[2][0]."<br>";
+    echo $student[0][3]." ".$student[1][1]." ".$student[2][0]."<br>";
+    echo $student[0][4]." ".$student[1][1]." ".$student[2][0]."<br>";
 
 ?>
