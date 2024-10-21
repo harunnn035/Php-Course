de49fbf122fb0407facf34cbe8150527f096260a
harunnn035 <harundag0101@gmail.com>
2024-10-21 16:58:48 +0300

    Update 8.Donguler.php

--- 8.Donguler.php
+++ 8.Donguler.php
@@ -2,15 +2,16 @@
 
 
 //While
+echo "While <br/> ";
 $a = 0;
 
 while ($a <= 10) {
-    echo "$a . merhaba <br/>";
-    $a++;
+    echo "$a . merhaba <br/> ";
+    $a++;   
 }
 
 //İç İçe While
-
+echo "<br/> İç İçe While <br/> ";
 $a = 1;
 $b = 1;
 
@@ -48,13 +49,22 @@ do{
     echo "sayı 6'dan küçük sayi=$a <br/>";
     $a++;
 }while ($a<6);
-echo "sayı 6'dan büyük sayi=$a";
+echo "sayı 6'dan büyük sayi=$a <br/> ";
 
 //For
 
 echo "<br/> For Döngüsü <br/>";
 for($a=0; $a<=10; $a++){
     echo "sayı=$a <br/>";
+}
+
+
+
+echo "<br/> Diziler İle For Döngüsü <br/>";
+
+$name=["ahmet","mehemt","arda","can","cancan","kadr","besr","harn"];
+for($a=0; $a < count($name); $a++){
+    echo "name=$name[$a] <br/> ";
 }
 
 
